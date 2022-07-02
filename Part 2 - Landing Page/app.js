f72b34876060ed2606dfa59cb9f1e9c58882b893
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-07-03 00:16:32 +0800

    reset template

--- Part 2 - Landing Page/app.js
+++ Part 2 - Landing Page/app.js
@@ -1,58 +1,60 @@
-//  Define Global Variables.
-const navigation = document.getElementById('navbar__list');
-const sections = document.querySelectorAll('section');
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
 
 // build the nav
-const navBuilder = () => {
-    let navUI = '';
 
-    sections.forEach(section => {
-        const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
-        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;   
-    });
-    navigation.innerHTML = navUI;  
-};
-navBuilder();
 
 // Add class 'active' to section when near top of viewport
-const offset = (section) => {
-    return Math.floor(section.getBoundingClientRect().top);
-};
 
-const removeActive = (section) => {
-    section.classList.remove('your-active-class');
-};
-
-const addActive = (conditional, section) => {
-    if (conditional) {
-        section.classList.add('your-active-class');
-    };
-};
-
-const sectionActivation = () => {
-    sections.forEach(section => {
-        const elementOffset = offset(section);
-
-        inviewport = () => elementOffset < 150 && elementOffset >= -150;
-
-        removeActive(section);
-        addActive(inviewport(), section);
-    });
-};
-
-
-window.addEventListener('scroll', sectionActivation);
 
 // Scroll to anchor ID using scrollTO event
-const links = document.querySelectorAll('.navbar__menu a');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        for (i = 0; i < sections; i++) {
-            sections[i].addEventListener('click', sectionsScroll(link));
-        }
-    });
-});
 
-// Scroll to section item
-document.querySelectorAll('')
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
