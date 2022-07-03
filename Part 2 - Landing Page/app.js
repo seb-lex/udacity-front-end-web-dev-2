eaf3de93f5dd50c8954d42756e8e8ba0de4ae534
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-07-03 12:31:02 +0800

    grab ul and section elements in order to build nav menu

--- Part 2 - Landing Page/app.js
+++ Part 2 - Landing Page/app.js
@@ -1,42 +1,6 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// Define Global Variables
+const navigation = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
 
 // build the nav
 
