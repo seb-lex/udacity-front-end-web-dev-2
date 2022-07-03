841964b553d7cdc4c03d99456fb53211a8f09d45
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-07-03 13:04:37 +0800

    revert to original file structure; include readme.md

--- Part 2 - Landing Page/js/app.js
+++ Part 2 - Landing Page/js/app.js
@@ -1,58 +1,34 @@
-//  Define Global Variables.
+// Define Global Variables
 const navigation = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
 // build the nav
-const navBuilder = () => {
-    let navUI = '';
+const navMenu = () => {
+    let navList = '';
 
     sections.forEach(section => {
         const sectionID = section.id;
         const sectionDataNav = section.dataset.nav;
-        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;   
+        navList += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
     });
-    navigation.innerHTML = navUI;  
+    navigation.innerHTML = navList;
 };
-navBuilder();
+navMenu();
 
 // Add class 'active' to section when near top of viewport
-const offset = (section) => {
-    return Math.floor(section.getBoundingClientRect().top);
-};
 
-const removeActive = (section) => {
-    section.classList.remove('your-active-class');
-};
-
-const addActive = (conditional, section) => {
-    if (conditional) {
-        section.classList.add('your-active-class');
-    };
-};
-
-const sectionActivation = () => {
-    sections.forEach(section => {
-        const elementOffset = offset(section);
-
-        inviewport = () => elementOffset < 150 && elementOffset >= -150;
-
-        removeActive(section);
-        addActive(inviewport(), section);
-    });
-};
-
-
-window.addEventListener('scroll', sectionActivation);
 
 // Scroll to anchor ID using scrollTO event
-const links = document.querySelectorAll('.navbar__menu a');
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        for (i = 0; i < sections; i++) {
-            sections[i].addEventListener('click', sectionsScroll(link));
-        }
-    });
-});
 
-// Scroll to section item
-document.querySelectorAll('')
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
