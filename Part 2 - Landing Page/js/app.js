1bde380e9eba233e394d144044d3f438bb45adcb
Sebastian <105150623+sebz996@users.noreply.github.com>
2022-07-03 13:08:08 +0800

    readd original template comments

--- Part 2 - Landing Page/js/app.js
+++ Part 2 - Landing Page/js/app.js
@@ -1,6 +1,45 @@
-// Define Global Variables
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
 const navigation = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
 
 // build the nav
 const navMenu = () => {
